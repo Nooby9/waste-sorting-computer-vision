39800cf2a170dfe0ff91b29940b85e70c020cc84
Nooby9 <1330762153@qq.com>
2024-11-14 02:57:20 -0800

    Trained on image size 832 with 191 epochs, mAP50 0.2

--- yolo11_waste_sorting/runModel.py
+++ yolo11_waste_sorting/runModel.py
@@ -4,11 +4,11 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ''
 from ultralytics import YOLO
 
 # Load the trained ONNX model
-model = YOLO("runs/detect/train/weights/best.onnx")
+model = YOLO("runs/detect/train_res_832/weights/best.pt")
 
 
 # Perform object detection on a new image
-results = model("000080.jpg")
+results = model("sample_run_images/organic_waste.jpg")
 
 # Save the annotated result
-results[0].save("000080.png")
+results[0].save("sample_run_images/organic_waste_annotated.png")
--- yolo11_waste_sorting/waste_sorting_training.py
+++ yolo11_waste_sorting/waste_sorting_training.py
@@ -6,18 +6,14 @@ model = YOLO("yolo11n.pt")
 # Train the model
 train_results = model.train(
     data="./sampled_dataset/data.yaml",  # path to dataset YAML
-    epochs=500,  # number of training epochs
-    imgsz=1024,  # training image size
+    epochs=100,  # number of training epochs
+    imgsz=832,  # training image size
     batch=4,
     device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
 )
 
 # Evaluate model performance on the validation set
 metrics = model.val(iou=0.5)
-
-# Perform object detection on an image
-results = model("plastic_bottle.jpg")
-results[0].save("plastic_bottle_annotated.png")
 
 # Export the model to ONNX/TensorRT format
 path = model.export(format="ONNX")  # return path to exported model
